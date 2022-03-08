5e65159027c6ea9b7073afd4f64e92ea845d8fb8
noaione <noaione0809@gmail.com>
2022-03-08 22:15:27 +0700

    chore: add license to file head

--- comix/__init__.py
+++ comix/__init__.py
@@ -1,3 +1,12 @@
+"""
+comix
+~~~~~~
+All of comix modules, it can also be imported!
+
+:copyright: (c) 2022-present noaione
+:license: MIT, see LICENSE for more details.
+"""
+
 from .amz import AmazonAuth
 from .cli import main
 from .client import CmxClient
